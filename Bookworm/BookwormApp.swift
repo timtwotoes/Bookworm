cd777d8cf913422d91b9edbc66e163de83ca8857
Tim Wolff <timtwotoes@me.com>
2023-05-02 18:33:23 +0200

    Initial Commit

---
+++ Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by Tim on 02/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
